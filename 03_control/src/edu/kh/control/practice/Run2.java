c3e0e3f65b5195ac2fa3f93078480b99573bca13
COQUTE <kimjiho246@naver.com>
2025-02-14 15:32:13 +0900

    주석

--- 03_control/src/edu/kh/control/practice/Run2.java
+++ 03_control/src/edu/kh/control/practice/Run2.java
@@ -7,18 +7,18 @@ public class Run2 {
 		LoopPractice lope = new LoopPractice();
 		
 		lope.practice1();
-//		lope.practice2();
-//		lope.practice3();
-//		lope.practice4();
-//		lope.practice5();
-//		lope.practice6();
-//		lope.practice7();
-//		lope.practice8();
-//		lope.practice9();
-//		lope.practice10();
-//		lope.practice11();
-//		lope.practice12();
-//		lope.practice13();
+		lope.practice2();
+		lope.practice3();
+		lope.practice4();
+		lope.practice5();
+		lope.practice6();
+		lope.practice7();
+		lope.practice8();
+		lope.practice9();
+		lope.practice10();
+		lope.practice11();
+		lope.practice12();
+		lope.practice13();
 	}
 
 }
